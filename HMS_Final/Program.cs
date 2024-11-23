914487a97d16ecfb58c14a7acca8e4bc1e99a550
mustafashahzadd <135046795+mustafashahzadd@users.noreply.github.com>
2024-11-23 22:48:14 +0500

    model added

--- HMS_Final/Program.cs
+++ HMS_Final/Program.cs
@@ -1,8 +1,17 @@
+using AppointmentManagementSystem.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Configure DbContext with SQL Server
+builder.Services.AddDbContext<AMSDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
